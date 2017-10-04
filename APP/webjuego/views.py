455931402f5d9a3c14c93d215ea0c811c9e6aa04
Juan Jurado Pérez <juan.jurado.perez@gmail.com>
2017-10-04 13:29:23 +0200

    vistas

--- APP/webjuego/views.py
+++ APP/webjuego/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from .models import *
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 #from django.forms import ModelForm
 # Create your views here.
@@ -9,3 +11,21 @@ from .models import *
 class Juegos(ListView):
 	model = Juego
 	template_name = "inicio.html"
+	context_object_name = 'lista_de_juegos'
+	queryset = Juego.objects.all()
+	
+	
+	
+class Usuarioscreate(CreateView):
+	model=Usuario
+	fields = ['nombre','gmail']
+
+class Usuariosupdate(UpdateView):
+	model=Usuario
+	fields = ['nombre','gmail']
+	
+class Usuariosdelete(DeleteView):
+	model=Usuario
+	success_url = reverse_lazy('Usuario-list')
+	
+
