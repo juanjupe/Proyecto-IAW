db3c97bf7113ee5db616cd00202a0d53f0700bd8
Juan Jurado Pérez <juan.jurado.perez@gmail.com>
2017-10-05 19:26:13 +0200

    pl

--- APP/videojuegos/forms.py
+++ APP/videojuegos/forms.py
@@ -1,1 +1,10 @@
 from django import forms
+from webjuego.models import Usuario
+
+
+class UsuarioForm(forms.ModelForm):
+	class Meta:
+		model=Usuario
+		fields=[
+		'nombre',
+		'gmail']
--- APP/webjuego/models.py
+++ APP/webjuego/models.py
@@ -11,14 +11,16 @@ class Genero (models.Model):
 	tipo = models.CharField(max_length=50)
 class Creador(models.Model):
 	patrocinador=models.CharField(max_length=50)
-
+class Plataforma(models.Model):
+	plataforma=models.CharField(max_length=50)
+	
 class Juego (models.Model):
 	nombre = models.CharField(max_length=50)
 	genero=models.ManyToManyField(Genero,default=0)
 	creador=models.ForeignKey(Creador,default=0)
-class Plataforma(models.Model):
-	plataforma=models.CharField(max_length=50)
-	juego=models.ManyToManyField(Juego,default=0)	
+	plataforma=models.ManyToManyField(Plataforma,default=0)
+
+		
 class Comentario(models.Model):
 	comentario = models.CharField(max_length=50,primary_key=True)
 	usuario=models.ManyToManyField(Usuario,default=0)
